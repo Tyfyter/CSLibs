327adf4c56d794f3936875518cf50eb763175e19
Tyfyter <tyfyter2616@gmail.com>
2021-02-11 01:35:24 -0500

    Update MultiTileUtils.cs

--- MultiTileUtils.cs
+++ MultiTileUtils.cs
@@ -1,5 +1,52 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
 namespace Tyfyter.Utils {
     public static class MultiTileUtils {
+        private static void chek(Mod moid) {
+            moid.Logger.Info("yerlp");
+        }
+        public static Point GetRelativeOriginCoordinates(TileObjectData objectData, Tile tile) {
+            int frameX = tile.frameX % objectData.CoordinateFullWidth;
+            int frameWidth = objectData.CoordinateWidth + objectData.CoordinatePadding;
+            int frameY = 0;
+            for(int y = tile.frameY; y > 0; y-=objectData.CoordinateHeights[frameY] + objectData.CoordinatePadding) {
+                frameY++;
+            }
+            return new Point(objectData.Origin.X - (frameX / frameWidth), objectData.Origin.Y - frameY);
+        }
 
+        public static Point GetFrameFromCoordinates(TileObjectData objectData, Point Coords) {
+            int frameX = Coords.X * objectData.CoordinateFullWidth;
+            int frameY = 0;
+            for(int y = Coords.Y; y > 0; y--) {
+                frameY+=objectData.CoordinateHeights[frameY] + objectData.CoordinatePadding;
+            }
+            return new Point(frameX, frameY);
+        }
+
+        public static int GetStyleWidth(ushort type) {
+            TileObjectData objectData = TileObjectData.GetTileData(type, 0);
+            return objectData.CoordinateFullWidth;
+        }
+        public static void AggressivelyPlace(Point Coords, ushort type, int style) {
+            TileObjectData objectData = TileObjectData.GetTileData(type, style);
+            int frameX = style * objectData.CoordinateFullWidth;
+            int frameY = 0;
+            Tile tile;
+            for(int y = 0; y < objectData.Height; y++) {
+                frameX = style * objectData.CoordinateFullWidth;
+                for(int x = 0; x < objectData.Width; x++) {
+                    tile = Main.tile[Coords.X + x, Coords.Y + y];
+                    tile.ResetToType(type);
+                    tile.frameX = (short)frameX;
+                    tile.frameY = (short)frameY;
+                    frameX+=objectData.CoordinateWidth + objectData.CoordinatePadding;
+                }
+                frameY+=objectData.CoordinateHeights[y] + objectData.CoordinatePadding;
+            }
+        }
     }
 }
