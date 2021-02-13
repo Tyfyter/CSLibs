4f5598b34d6d632a08dcf82c5d9a79ae07b9e796
Tyfyter <tyfyter2616@gmail.com>
2021-02-13 01:51:10 -0500

    Create NonFishUtil.cs

---
+++ NonFishUtil.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Tyfyter.Utils {
+    
+    public sealed class NonFishItem : ModItem {
+        public override string Texture => "Terraria/Item_2290";
+        public static event Action ResizeItemArrays;
+        public static event Action ResizeOtherArrays;
+        public override bool IsQuestFish() {
+            ResizeItemArrays();
+            ResizeItemArrays = null;
+            return false;
+        }
+    }
+}
