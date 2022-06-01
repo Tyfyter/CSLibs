cba1735015382e323ce72807bddce77d7c9f8713
Tyfyter <tyfyter2616@gmail.com>
2022-06-01 00:09:04 -0400

    GraphicsUtils

--- IDs.cs
+++ IDs.cs
@@ -288,7 +288,7 @@ namespace Tyfyter.Utils.ID {
 		///</summary>
 		public const int Creeper = 55;
 		///<summary>
-		/// Moves directly towards player, gaining momentum. Emits blue-ish particles.
+		/// Moves directly towards player, gaining momentum. Emits blueish particles.
 		///</summary>
 		public const int Dungeon_Spirit = 56;
 		///<summary>
